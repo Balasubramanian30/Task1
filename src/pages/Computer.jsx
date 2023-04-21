163d76cea3ebddcdf7efcd4e951e2b7f4899d441
Balasubramanian30 <balumarketing2002@gmail.com>
2023-04-21 20:36:32 +0530

    Task

--- src/pages/Computer.jsx
+++ src/pages/Computer.jsx
@@ -5,17 +5,24 @@ function Computer() {
   return (
     <Fragment>
       <Header />
-      <div className="lap1">
-        <div className="img1">
-          <img src="./ascets/images/lap1.webp" alt="img" />
+      <div class="lap1">
+        <div class="img1">
+            <img src="images/lap1.webp" alt="ak"/>
         </div>
-        <div className="img1">
-          <img src="./ascets/images/lap2.webp" alt="img" />
+        <p class="sys">System 1</p>
+        <div class="img1">
+            <img src="images/lap2.webp" alt="ak" />
+
         </div>
-        <div className="img1">
-          <img src="./ascets/images/lap3.webp" alt="img" />
+        <p class="sys">System 2</p>
+        <div class="img1">
+            <img src="images/lap3.webp" alt="ak" />
         </div>
-      </div>
+        <p class="sys">System 3</p>
+
+
+
+    </div>
     </Fragment>
   );
 }
